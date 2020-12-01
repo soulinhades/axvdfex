31f6929b72ae5370dcb5088de2b89814e1201f95
Ubuntu <ubuntu@zingflow.zing>
2020-12-01 06:13:40 +0000

    phashing

--- axvdfsearchex2.py
+++ axvdfsearchex2.py
@@ -22,9 +22,9 @@ for i in range(0,len(keys)):
         sigy = mdic[keys[j]]
         match, seq, sj = axvdfex.search_video3xx(sigx,sigy,int(sys.argv[3]))
         if seq > m:
-            print(keys[i], keys[j], seq, len(sigx), len(sigy), "SUB" if sj!=0 else "")
+            print(keys[i], keys[j], seq, len(sigx), len(sigy), sj)
             print("-----------------------------------------------------------------------------------")
-            print("%s,%s,%d,%d,%d,%s" % (keys[i],keys[j],seq,len(sigx),len(sigy),"SUB" if sj!=0 else ""),file=f)
+            print("%s,%s,%d,%d,%d,%s" % (keys[i],keys[j],seq,len(sigx),len(sigy),sj),file=f)
             #if sj != 0: input("Press Enter to continue...")
 end = datetime.now()
 print(count, end - start)
